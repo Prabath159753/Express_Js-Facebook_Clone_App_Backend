377e33e1bfa2415c8ebbea2f11027e78db6e8f55
Kavishka <kavishkaprabath21@gmail.com>
2022-08-26 21:56:23 +0530

    add router of save user

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const User = require('../model/user.model')
 
 app.use(express.json())
 
-
+// get all users
 router.get('/',async (req, res) =>{
     try {
         const register = await User.find()
@@ -16,6 +16,7 @@ router.get('/',async (req, res) =>{
     }
 })
 
+// get user for id
 router.get('/:id',async (req, res) =>{
     try {
         const register = await User.findById(req.params.id)
@@ -25,6 +26,7 @@ router.get('/:id',async (req, res) =>{
     }
 })
 
+// get user for email and password
 router.get('/:email/:password',async (req, res) =>{
     try {
         const register = await User.findOne({ email: req.params.email, password: req.params.password })
@@ -32,6 +34,27 @@ router.get('/:email/:password',async (req, res) =>{
     }catch (error) {
         res.send('Error : '+ error)
     }
+})
+
+// save user
+router.post('/',async (req,res) => {
+    const user = new User({
+        firstName:req.body.firstName,
+        surname:req.body.surname,
+        gender:req.body.gender,
+        dateOfBirth:req.body.dateOfBirth,
+        password:req.body.password,
+        phoneNumber:req.body.phoneNumber,
+        email:req.body.email,
+    })
+
+    try {
+        const register = await user.save()
+        res.json(register)
+    }catch (error) {
+        res.send('Error : '+ error)
+    }
+
 })
 
 
