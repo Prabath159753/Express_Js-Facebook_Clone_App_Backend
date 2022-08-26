d449db10ac709465e241ceeba2e466021e7850dd
Kavishka <kavishkaprabath21@gmail.com>
2022-08-26 23:44:43 +0530

    add get all post router to post.js file

--- routes/post.js
+++ routes/post.js
@@ -6,6 +6,16 @@ const Posts = require('../model/post.model')
 
 app.use(express.json())
 
+// get all post
+router.get('/',async (req, res) =>{
+    try {
+        const posts = await Posts.find()
+        res.json(posts)
+    }catch (error) {
+        res.send('Error : '+ error)
+    }
+})
+
 
 
 
