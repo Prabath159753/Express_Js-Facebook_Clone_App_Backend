069858784293b97548a83a416e493e643e8409bf
Kavishka <kavishkaprabath21@gmail.com>
2022-08-26 10:10:51 +0530

    update app.js file

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app=express()
+const port = 4001
+
+const url = 'mongodb://127.0.0.1/facebookClone'
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on("open", () => {
+    console.log('MongoDB connected!');
+})
+
+app.use(express.json())
+
+
+app.listen(port, () => {
+    console.log(`app starting on ${port}`);
+})
