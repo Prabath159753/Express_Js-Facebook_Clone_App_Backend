07a00479e5e96034df068ba4df3a1637933cea22
Kavishka <kavishkaprabath21@gmail.com>
2022-08-27 02:39:18 +0530

    add login.js file

---
+++ routes/login.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+
+const Login = require('../model/login.model')
+const User = require('../model/user.model')
+
+
+app.use(express.json())
+
+router.post('/', async (req, res) => {
+    try {
+        const userCount=await User.findOne({email: new RegExp('^'+req.body.email+'$', "i")}).count();
+
+        if (userCount!==0){
+            const user=await User.findOne({email: new RegExp('^'+req.body.email+'$', "i")});
+            console.log(user);
+
+            if (await user.password==req.body.password){
+                const login=new Login({
+                    email:req.body.email,
+                    password : req.body.password,
+
+                });
+                const response=await login.save()
+                res.send(user.firstName +"Successful logged in")
+            
+            }else {
+                res.send("Wrong Password. Please Input Correct Password.Try again")
+            }
+
+        }else {
+            res.send("User not exist please check email")
+        }
+
+    }catch (error){
+        res.send('Error : '+ error)
+    }
+})
+
+module.exports = router
